0baded62a3ecf65c300cb420d83481623ff044fc
Alba Mendez <alba.mendez@dolby.com>
2022-12-20 14:11:16 +0100

    more unit tests

--- test/BitReader.test.ts
+++ test/BitReader.test.ts
@@ -1,7 +1,20 @@
 import BitReader from "../lib/BitReader.js"
 
 describe('BitReader', () => {
-	it('basic tests', () => {
+	it('validates constructor parameters', () => {
+		expect(() => new BitReader(Uint8Array.of(0), 8, 8))
+			.not.toThrow()
+		expect(() => new BitReader(Uint8Array.of(0), 0, 0))
+			.not.toThrow()
+		expect(() => new BitReader(Uint8Array.of(0), 8, 7))
+			.toThrow()
+		expect(() => new BitReader(Uint8Array.of(0), 8, 9))
+			.toThrow()
+		expect(() => new BitReader(Uint8Array.of(0), -1, 8))
+			.toThrow()
+	})
+
+	it('reads a fixed bit amount', () => {
 		const data = Uint8Array.of(0b10000011, 0b00000110, 0b00001100, 0b00011000, 0b00110000, 0b01100000, 0b11000001)
 		const reader = new BitReader(data)
 		for (let i = 0; i < 8; i++) {
@@ -12,9 +25,14 @@ describe('BitReader', () => {
 		expect(reader.offset).toBe(data.length * 8)
 		expect(reader.length).toBe(data.length * 8)
 		expect(() => reader.read1()).toThrow('unexpected EOF')
+
+		expect(new BitReader(Uint8Array.of(0, 1, 2, 3, 4)).read(32))
+			.toBe(0x00010203)
+		expect(() => new BitReader(Uint8Array.of(0, 1, 2, 3, 4)).read(33))
+			.toThrow()
 	})
 
-	it('ue(v)', () => {
+	it('parses ue(v)', () => {
 		const cases: [number, number][] = [
 			[1,    0b1    ],
 			[3,   0b010   ],
@@ -37,7 +55,7 @@ describe('BitReader', () => {
 		}
 	})
 
-	it('se(v) zig-zag encoding', () => {
+	it('parses se(v) (zig-zag encoding)', () => {
 		const reader = new BitReader(Uint8Array.of())
 		for (const [i, o] of [0, 1, -1, 2, -2, 3, -3].entries()) {
 			reader.readExp = () => i
--- test/SPS.test.ts
+++ test/SPS.test.ts
@@ -76,5 +76,93 @@ describe('SPS parsing', () => {
 				bitstream_restriction: undefined,
 			},
 		})
+
+		fullParseSPS('6764001eacb2010020f3cf80b5060606a50000030001000003003c9f162e48', {
+			profile_idc: 100,
+			profile_compatibility: 0,
+			level_idc: 30,
+			seq_parameter_set_id: 0,
+
+			chroma_format_idc: 1,
+			separate_colour_plane_flag: undefined,
+			bit_depth_luma_minus8: 0,
+			bit_depth_chroma_minus8: 0,
+			qpprime_y_zero_transform_bypass_flag: false,
+			seq_scaling_matrix: undefined,
+
+			log2_max_frame_num_minus4: 0,
+			pic_order_cnt_type: 2,
+			max_num_ref_frames: 3,
+			gaps_in_frame_num_value_allowed_flag: false,
+			pic_width_in_mbs_minus1: 31,
+			pic_height_in_map_units_minus1: 31,
+			frame_mbs_only_flag: true,
+			mb_adaptive_frame_field_flag: undefined,
+			direct_8x8_inference_flag: true,
+			frame_cropping: { left_offset: 0, right_offset: 6, top_offset: 0, bottom_offset: 6 },
+
+			vui_parameters: {
+				aspect_ratio_info: { aspect_ratio_idc: 1 },
+				overscan_info: undefined,
+				video_signal_type: {
+					video_format: 5,
+					video_full_range_flag: false,
+					colour_description: {
+						colour_primaries: 6,
+						matrix_coefficients: 6,
+						transfer_characteristics: 6,
+					},
+				},
+				chroma_loc_info: {
+					chroma_sample_loc_type_top_field: 1,
+					chroma_sample_loc_type_bottom_field: 1,
+				},
+				timing_info: {
+					num_units_in_tick: 1,
+					time_scale: 60,
+					fixed_frame_rate_flag: true,
+				},
+				nal_hrd_parameters: undefined,
+				vcl_hrd_parameters: undefined,
+				low_delay_hrd_flag: undefined,
+				pic_struct_present_flag: true,
+				bitstream_restriction: {
+					motion_vectors_over_pic_boundaries_flag: true,
+					max_bytes_per_pic_denom: 0,
+					max_bits_per_mb_denom: 0,
+					log2_max_mv_length_horizontal: 10,
+					log2_max_mv_length_vertical: 10,
+					max_num_reorder_frames: 0,
+					max_dec_frame_buffering: 3,
+				},
+			}
+		})
+	})
+
+	it('fails on invalid inputs', () => {
+		expect(() => parseSPS(Buffer.from('64001fad84010c20086100430802184010c200843b502802dd35010101400145d8c04c4b4025', 'hex')))
+			.not.toThrow()
+		// invalid trailing
+		expect(() => parseSPS(Buffer.from('64001fad84010c20086100430802184010c200843b502802dd35010101400145d8c04c4b4024', 'hex')))
+			.toThrow()
+		expect(() => parseSPS(Buffer.from('64001fad84010c20086100430802184010c200843b502802dd35010101400145d8c04c4b4026', 'hex')))
+			.toThrow()
+		expect(() => parseSPS(Buffer.from('64001fad84010c20086100430802184010c200843b502802dd35010101400145d8c04c4b402500', 'hex')))
+			.toThrow()
+		// valid trailing, but extra data
+		expect(() => parseSPS(Buffer.from('64001fad84010c20086100430802184010c200843b502802dd35010101400145d8c04c4b402401', 'hex')))
+			.toThrow()
+		// cut short
+		expect(() => parseSPS(Buffer.from('64001fad84010c20086100430802184010', 'hex')))
+			.toThrow()
+	})
+
+	it('validates IDs', () => {
+		expect(validateSPSId(0)).toBe(0)
+		expect(validateSPSId(31)).toBe(31)
+		expect(() => validateSPSId(-1)).toThrow()
+		expect(() => validateSPSId(32)).toThrow()
+		expect(() => validateSPSId(0.1)).toThrow()
+		expect(() => validateSPSId(NaN)).toThrow()
 	})
 })
