dfde04bcecd4fd1e2f64a8039cc325ac20f67025
SCG82 <scg082+github@gmail.com>
2022-12-22 01:30:36 +0000

    replace @types/jest with @jest/globals

--- test/sei/UserDataUnregistered.test.ts
+++ test/sei/UserDataUnregistered.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from "@jest/globals"
 import { decodeRBSP, NALUType, parseNALU, parseSEI, parseUuid, SEIMessageType } from "../../lib"
 import { parseUserDataUnregistered } from "../../lib/sei/UserDataUnregistered"
 
